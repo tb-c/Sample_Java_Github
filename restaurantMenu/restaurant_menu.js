e6416e9fe3a6df1c08ea81844e5a190c9fde684e
Tristan Brank <tristanbrank19@gmail.com>
2024-04-03 14:48:05 -0400

    practice task

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,19 @@
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// iteration through the array using the map() method
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+//iteration through the forEach method 
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+//iteration through the array using a for loop
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
